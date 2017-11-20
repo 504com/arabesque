b0696623690ae12b815d373b8c5f11315ea74f88
Mohammed BOUSTTA <mohamed.boustta@gmail.com>
2017-11-20 01:12:04 +0100

    removing white container - replace with transparence one

--- footer.php
+++ footer.php
@@ -1,9 +1,12 @@
-                    </div>
-                        <div class="clear"></div>
-                    </div>
-                <div class="footer">
-                    <div class="footerlink"> Copyright (c) 504com.com All rights reserved. <a href="http://arabesque.com">Website Design</a>. </div>
-                </div>
-        </div>
+<div class="clear"></div>
+</div>
+
+</div>
+<div class="clear"></div>
+<div class="footer">
+    <div class="footerlink"> Copyright (c) 504com.com All rights reserved. <a href="http://arabesque.com">Website Design</a>. </div>
+</div>
+<div class="clear"></div>
+</div>
 </body>
 </html>
