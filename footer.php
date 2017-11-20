f7ebf59eb888b9b5d7c270a0b740c5b3c34dc3f2
Mohammed BOUSTTA <mohamed.boustta@gmail.com>
2017-11-20 01:04:29 +0100

    [maj]

--- footer.php
+++ footer.php
@@ -1,12 +1,9 @@
-<div class="clear"></div>
-</div>
-<div class="clear"></div>
-</div>
-<div class="clear"></div>
-<div class="footer">
-    <div class="footerlink"> Copyright (c) 504com.com All rights reserved. <a href="http://arabesque.com">Website Design</a>. </div>
-</div>
-<div class="clear"></div>
-</div>
+                    </div>
+                        <div class="clear"></div>
+                    </div>
+                <div class="footer">
+                    <div class="footerlink"> Copyright (c) 504com.com All rights reserved. <a href="http://arabesque.com">Website Design</a>. </div>
+                </div>
+        </div>
 </body>
 </html>
